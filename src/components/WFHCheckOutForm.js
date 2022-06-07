b1986143d2e165264f85c14d080c9c1b9343de91
Marcus Gainey <marcusgainey@Marcuss-MacBook-Air.local>
2022-06-07 13:46:51 -0400

    updates to wfh checkout page

--- src/components/WFHCheckOutForm.js
+++ src/components/WFHCheckOutForm.js
@@ -8,21 +8,62 @@ import {
   MenuItem,
   FormControlLabel,
   Checkbox,
-  Alert
+  Alert,
+  Card,
+  Button,
+  Table,
+  TableContainer,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+  TextField,
 } from "@mui/material";
 import React from "react";
 
-const WFHCheckOutForm = ({ employees, newChecked, handleNewChecked }) => {
+const WFHCheckOutForm = ({
+  employees,
+  site,
+  currentEmployee,
+  newChecked,
+  handleSiteChange,
+  handleCurrentEmployeeChange,
+  handleNewChecked,
+  computer,
+  handleComputerChange,
+  computerYear,
+  handleComputerYearChange,
+  monitors,
+  handleMonitorsChange,
+  monitorQuantity,
+  handleMonitorQuantityChange,
+  monitor1Style,
+  handleMonitor1StyleChange,
+  monitor2Style,
+  handleMonitor2StyleChange,
+  headset,
+  handleHeadsetChange,
+  headsetMono,
+  handleHeadsetMonoChange,
+  headsetDuo,
+  handleHeadsetDuoChange,
+  keyboard,
+  handleKeyboardChange,
+  mouse,
+  handleMouseChange,
+}) => {
   const paperStyle = {
     padding: 20,
     height: "90vh",
     width: "66%",
     margin: "20px auto",
   };
+
   return (
     <>
       <Grid container align="center">
         <Paper style={paperStyle} elevation={10}>
+          {/* Header Start */}
           <Typography variant="h4" style={{ fontWeight: "bold" }}>
             Work From Home Inventory Checkout
           </Typography>
@@ -32,40 +73,230 @@ const WFHCheckOutForm = ({ employees, newChecked, handleNewChecked }) => {
               employee will be using outside of the office
             </Typography>
           </Grid>
+          {/* Form Start */}
           <Grid container spacing={2} align="left">
-            <Grid xs={7} item>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    checked={newChecked}
-                    onChange={handleNewChecked}
-                  />
-                }
-                label="New Inventory"
-              />
+            {/* Form Header Start */}
+            <Grid xs={12} item>
+              <Card>
+                <FormControl sx={{ minWidth: 120 }} size="small">
+                  <InputLabel id="site-select-label">Site</InputLabel>
+                  <Select
+                    labelId="site-select-label"
+                    id="site-select"
+                    value={site}
+                    onChange={handleSiteChange}
+                  >
+                    <MenuItem value={"New York Office"}>
+                      New York Office
+                    </MenuItem>
+                    <MenuItem value={"Ohio Office"}>Ohio Office</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl sx={{ minWidth: 120 }} size="small">
+                  <InputLabel id="employee-select-label">Employee</InputLabel>
+                  <Select
+                    labelId="employee-select-label"
+                    id="employee-select"
+                    value={currentEmployee}
+                    onChange={handleCurrentEmployeeChange}
+                  >
+                    {employees
+                      .filter((employee) => employee.site === site)
+                      .map((employee) => (
+                        <MenuItem value={employee.employeeName}>
+                          {employee.employeeName}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </Card>
             </Grid>
-            {
-              newChecked && (<Alert severity="info">Use only for Inventory not previously assigned. Checking this box will both add new inventory and assign it to a WFH employee</Alert>)
-            }
-            <Grid xs={6} item>
-              <FormControl fullWidth>
-                <InputLabel id="site-select-label">Site</InputLabel>
-                <Select labelId="site-select-label" id="site-select">
-                    <MenuItem>NYC Office</MenuItem>
-                    <MenuItem>Ohio Office</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid xs={6} item>
-              <FormControl fullWidth>
-                <InputLabel id="employee-select-label">Employee</InputLabel>
-                <Select labelId="employee-select-label" id="employee-select">
-                  {employees.map((employee) => (
-                    <MenuItem>{employee.employeeName}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+            {/* Form Table Header */}
+            <Grid xs={12} item>
+              <Card>
+                {newChecked && (
+                  <Alert severity="info">
+                    Only check "New Inventory" for Inventory not previously
+                    assigned. Checking this box will both add new inventory and
+                    assign it to a WFH employee
+                  </Alert>
+                )}
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell colSpan={3}>
+                          <FormControlLabel
+                            control={
+                              <Checkbox
+                                color="primary"
+                                checked={newChecked}
+                                onChange={handleNewChecked}
+                              />
+                            }
+                            label="New Inventory"
+                          />
+                        </TableCell>
+                        <TableCell align="left">
+                          <Button variant="contained">Check Out</Button>
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    {/* Form Table Body */}
+                    <TableBody>
+                      {/* Computer Infor */}
+                      <TableRow>
+                        <TableCell>
+                          <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label="Computer"
+                            checked={computer}
+                            onChange={handleComputerChange}
+                          />
+                        </TableCell>
+                        <TableCell colSpan={3}>
+                          <TextField
+                            label="Year Purchased"
+                            type="number"
+                            disabled={!computer}
+                            value={computerYear}
+                            onChange={handleComputerYearChange}
+                          />
+                        </TableCell>
+                      </TableRow>
+                      {/* Monitor Info */}
+                      <TableRow>
+                        <TableCell>
+                          <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label="Monitor"
+                            checked={monitors}
+                            onChange={handleMonitorsChange}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <FormControl sx={{ minWidth: 100 }} size="small">
+                            <InputLabel id="monitorQuantity-select-label">
+                              Quantity
+                            </InputLabel>
+                            <Select
+                              labelId="monitorQuantity-select-label"
+                              id="monitorQuantity-select"
+                              value={monitorQuantity}
+                              onChange={handleMonitorQuantityChange}
+                              disabled={!monitors}
+                            >
+                              <MenuItem value={1}>1</MenuItem>
+                              <MenuItem value={2}>2</MenuItem>
+                            </Select>
+                          </FormControl>
+                        </TableCell>
+                        <TableCell>
+                          <FormControl sx={{ minWidth: 180 }} size="small">
+                            <InputLabel id="monitor1Style-select-label">
+                              Monitor 1 Style
+                            </InputLabel>
+                            <Select
+                              labelId="monitor1Style-select-label"
+                              id="monitor1Style--select"
+                              value={monitor1Style}
+                              onChange={handleMonitor1StyleChange}
+                              disabled={!monitors}
+                            >
+                              <MenuItem value={"2022 Purchased"}>
+                                2022 Purchased
+                              </MenuItem>
+                              <MenuItem value={"Pre 2022 Purchased"}>
+                                Pre 2022 Purchased
+                              </MenuItem>
+                            </Select>
+                          </FormControl>
+                        </TableCell>
+                        <TableCell>
+                          <FormControl sx={{ minWidth: 180 }} size="small">
+                            <InputLabel id="monitor2Style-select-label">
+                              Monitor 2 Style
+                            </InputLabel>
+                            <Select
+                              labelId="monitor2Style-select-label"
+                              id="monitor2Style--select"
+                              value={monitor2Style}
+                              onChange={handleMonitor2StyleChange}
+                              disabled={monitorQuantity !== 2}
+                            >
+                              <MenuItem value={"2022 Purchased"}>
+                                2022 Purchased
+                              </MenuItem>
+                              <MenuItem value={"Pre 2022 Purchased"}>
+                                Pre 2022 Purchased
+                              </MenuItem>
+                            </Select>
+                          </FormControl>
+                        </TableCell>
+                      </TableRow>
+                      {/* Headset Info */}
+                      <TableRow>
+                        <TableCell>
+                          <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label="Headset"
+                            checked={headset}
+                            onChange={handleHeadsetChange}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <FormControlLabel
+                            control={
+                              <Checkbox
+                                color="primary"
+                                disabled={!headset || headsetDuo}
+                              />
+                            }
+                            checked={headsetMono}
+                            onChange={handleHeadsetMonoChange}
+                            label="Mono"
+                          />
+                        </TableCell>
+                        <TableCell colSpan={2}>
+                          <FormControlLabel
+                            control={
+                              <Checkbox
+                                color="primary"
+                                disabled={!headset || headsetMono}
+                              />
+                            }
+                            checked={headsetDuo}
+                            onChange={handleHeadsetDuoChange}
+                            label="Duo"
+                          />
+                        </TableCell>
+                      </TableRow>
+                      {/* Keyboard Info */}
+                      <TableRow>
+                        <TableCell colSpan={4}>
+                          <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label="Keyboard"
+                            checked={keyboard}
+                            onChange={handleKeyboardChange}
+                          />
+                        </TableCell>
+                      </TableRow>
+                      {/* Mouse Info */}
+                      <TableRow>
+                        <TableCell colSpan={4}>
+                          <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label="Wireless Mouse"
+                            checked={mouse}
+                            onChange={handleMouseChange}
+                          />
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Card>
             </Grid>
           </Grid>
         </Paper>
--- src/pages/WFHCheckOut.js
+++ src/pages/WFHCheckOut.js
@@ -4,7 +4,20 @@ import WFHCheckOutForm from "../components/WFHCheckOutForm";
 
 const WFHCheckOut = () => {
   const [employees, setEmployees] = useState([]);
+  const [site, setSite] = useState("");
+  const [currentEmployee, setCurrentEmployee] = useState("");
   const [newChecked, setNewChecked] = useState(false);
+  const [computer, setComputer] = useState(false);
+  const [computerYear, setComputerYear] = useState('2022');
+  const [monitors, setMonitors] = useState(false);
+  const [monitorQuantity, setMonitorQuantity] = useState(0);
+  const [monitor1Style, setMonitor1Style] = useState("");
+  const [monitor2Style, setMonitor2Style] = useState("");
+  const [headset, setHeadset] = useState(false);
+  const [headsetMono, setHeadsetMono] = useState(false);
+  const [headsetDuo, setHeadsetDuo] = useState(false);
+  const [keyboard, setKeyboard] = useState(false);
+  const [mouse, setMouse] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:3001/employees")
@@ -12,18 +25,89 @@ const WFHCheckOut = () => {
       .then((data) => setEmployees(data));
   }, []);
 
+  const handleSiteChange = (e) => {
+    setSite(e.target.value);
+  };
+  const handleCurrentEmployeeChange = (e) => {
+    setCurrentEmployee(e.target.value);
+  };
   const handleNewChecked = () => {
     setNewChecked((prev) => !prev);
+  };
+  const handleComputerChange = () => {
+    setComputer((prev) => !prev);
+  };
+  const handleComputerYearChange = (e) => {
+    setComputerYear(e.target.value);
   }
-
+  const handleMonitorsChange = () => {
+    setMonitorQuantity(0);
+    setMonitor1Style("");
+    setMonitor2Style("")
+    setMonitors((prev) => !prev);
+  };
+  const handleMonitorQuantityChange = (e) => {
+    setMonitorQuantity(e.target.value);
+  }
+  const handleMonitor1StyleChange = (e) => {
+    setMonitor1Style(e.target.value);
+  }
+  const handleMonitor2StyleChange = (e) => {
+    setMonitor2Style(e.target.value);
+  }
+  const handleHeadsetChange = () => {
+    setHeadsetDuo(false);
+    setHeadsetMono(false);
+    setHeadset((prev) => !prev);
+  };
+  const handleHeadsetMonoChange = () => {
+    setHeadsetMono((prev) => !prev);
+  };
+  const handleHeadsetDuoChange = () => {
+    setHeadsetDuo((prev) => !prev);
+  };
+  const handleKeyboardChange = () => {
+    setKeyboard((prev) => !prev);
+  };
+  const handleMouseChange = () => {
+    setMouse((prev) => !prev);
+  };
+  
+  const formProps = {
+    employees,
+    site,
+    currentEmployee,
+    newChecked,
+    handleSiteChange,
+    handleCurrentEmployeeChange,
+    handleNewChecked,
+    computer,
+    handleComputerChange,
+    computerYear,
+    handleComputerYearChange,
+    monitors,
+    handleMonitorsChange,
+    monitorQuantity,
+    handleMonitorQuantityChange,
+    monitor1Style,
+    handleMonitor1StyleChange,
+    monitor2Style,
+    handleMonitor2StyleChange,
+    headset,
+    handleHeadsetChange,
+    headsetMono,
+    handleHeadsetMonoChange,
+    headsetDuo,
+    handleHeadsetDuoChange,
+    keyboard,
+    handleKeyboardChange,
+    mouse,
+    handleMouseChange,
+  };
   return (
     <>
       <NavBar />
-      <WFHCheckOutForm
-        employees={employees}
-        newChecked={newChecked}
-        handleNewChecked={handleNewChecked}
-      />
+      <WFHCheckOutForm {...formProps} />
     </>
   );
 };
